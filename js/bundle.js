d12b87292cbbbd9a4540233a7ccb8b1cd119b3c7
Couto1411 <gabriel.couto14@hotmail.com>
2022-07-12 00:10:08 -0300

    trying something in my language

--- js/bundle.js
+++ js/bundle.js
@@ -195,25 +195,25 @@ function handleRes(data) {
     // Create each article using map
     let article, articleContent;
     dataArray.map((story) => {
-
-        // Construct content of each with template literal              
-        articleContent = `
-        <div class="row pb-2 border-bottom border-secondary">
-            <div class="col-lg-4 col-md-6">
-                <img class="img-fluid" src="${story[1].urlToImage}">
+            // Construct content of each with template literal              
+            articleContent = `
+            <div class="row pb-2 border-bottom border-secondary">
+                <div class="col-lg-4 col-md-6">
+                    <img class="img-fluid" src="${story[1].urlToImage}">
+                </div>
+                <div class="col-lg-8 col-md-6">
+                    <a href="${story[1].url}">${story[1].title}</a>
+                    <ul class="hidden-mobile">
+                        <li>${story[1].description}</li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-lg-8 col-md-6">
-                <a href="${story[1].url}">${story[1].title}</a>
-                <ul class="hidden-mobile">
-                    <li>${story[1].description}</li>
-                </ul>
-            </div>
-        </div>
-        `
-        article = document.createElement('article');
-        article.innerHTML = articleContent;
-        if (story[1].type == "article") {
-            storiesContainer.append(article);
+            `
+            article = document.createElement('article');
+            article.innerHTML = articleContent;
+            console.log(story[1].urlToImage);
+            if (story[1].type == "article") {
+                storiesContainer.append(article);
         }
    })
 }
--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
-const reponse =fetch("https://content.guardianapis.com/search?q=sustainability&show-fields=all&show-blocks=body&api-key=test").then(
-    res => res.json()
-).then(
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('8f27c21012e148c19e8b6a8277cec24d',{ corsProxyUrl: 'https://cors-anywhere.herokuapp.com/' });
+newsapi.v2.everything({
+    q: 'sustentabilidade',
+    sortBy: 'publishedAt'
+}).then(
     res => handleRes(res)
-).catch(ex =>
-    console.error(ex)
 );
 const storiesContainer = document.getElementById('stories-container');
 const storyContainer = document.getElementById('story-container');
 function handleRes(data) {
     // Access fetched data as array
-    let dataArray = Object.entries(data.response.results);
+    let dataArray = Object.entries(data.articles);
 
     // Create each article using map
     let article, articleContent;
     dataArray.map((story) => {
-
-        // Construct content of each with template literal              
-        articleContent = `
-        <div class="row pb-2 border-bottom border-secondary">
-            <div class="col-lg-4 col-md-6">
-                <img class="img-fluid" src="${story[1].fields.thumbnail}">
+            // Construct content of each with template literal              
+            articleContent = `
+            <div class="row pb-2 border-bottom border-secondary">
+                <div class="col-lg-4 col-md-6">
+                    <img class="img-fluid" src="${story[1].urlToImage}">
+                </div>
+                <div class="col-lg-8 col-md-6">
+                    <a href="${story[1].url}">${story[1].title}</a>
+                    <ul class="hidden-mobile">
+                        <li>${story[1].description}</li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-lg-8 col-md-6">
-                <a href="${story[1].webUrl}">${story[1].webTitle}</a>
-                <ul class="hidden-mobile">
-                    <li>${story[1].fields.trailText}</li>
-                </ul>
-            </div>
-        </div>
-        `
-        article = document.createElement('article');
-        article.innerHTML = articleContent;
-        if (story[1].type == "article") {
-            storiesContainer.append(article);
+            `
+            article = document.createElement('article');
+            article.innerHTML = articleContent;
+            console.log(story[1].urlToImage);
+            if (story[1].type == "article") {
+                storiesContainer.append(article);
         }
    })
 }
